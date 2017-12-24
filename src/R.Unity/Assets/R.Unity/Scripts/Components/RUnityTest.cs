836f2589e499e334384a62c8c90362240a67d2b4
Ikiru Yoshizaki <cherishacquire@outlook.com>
2017-12-25 00:27:48 +0900

    Add Logger

--- src/R.Unity/Assets/R.Unity/Scripts/Components/RUnityTest.cs
+++ src/R.Unity/Assets/R.Unity/Scripts/Components/RUnityTest.cs
@@ -7,6 +7,7 @@ public class RUnityTest : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        //RUnity.Generator.Generator.UseGeneratorSceneNames = false;
         RUnity.Generator.Generator.Generate();
     }
 }
---
+++ src/R.Unity/Assets/R.Unity/Scripts/Generator/Logger.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+namespace RUnity.Generator
+{
+    public interface ILogger
+    {
+        void Info(object message);
+        void Warning(object message);
+        void Error(object message);
+        void Exception(Exception exception);
+    }
+
+    public class BlankLogger : ILogger
+    {
+        public void Error(object message)
+        {
+        }
+
+        public void Exception(Exception exception)
+        {
+        }
+
+        public void Info(object message)
+        {
+        }
+
+        public void Warning(object message)
+        {
+        }
+    }
+
+    public class UnityLogger : ILogger
+    {
+        public void Error(object message)
+        {
+            Debug.LogError(message);
+        }
+
+        public void Exception(Exception exception)
+        {
+            Debug.LogException(exception);
+        }
+
+        public void Info(object message)
+        {
+            Debug.Log(message);
+        }
+
+        public void Warning(object message)
+        {
+            Debug.LogWarning(message);
+        }
+    }
+}
+#endif
